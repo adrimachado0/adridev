cc06b7bd2cd90f2fcef97684756c440449533cd7
adrimachado0 <aadrimachado1@gmail.com>
2023-08-31 14:57:14 -0300

    sendmail

--- components/hooks/sendEmail.jsx
+++ components/hooks/sendEmail.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const sendEmail = async (email, text) => {
-  const res = await axios.post(process.env.EMAIL_URL, {
+  const res = await axios.post('https://portfolio-2023-psi-self.vercel.app/api/sendemail', {
     email:email,
     text:text,
   })
