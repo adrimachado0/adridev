38afd330357e03e70b2457e4782e006cf46757e5
adrimachado0 <aadrimachado1@gmail.com>
2023-08-31 15:55:33 -0300

    margin bottom added

--- components/contact/contact.jsx
+++ components/contact/contact.jsx
@@ -4,7 +4,7 @@ import Formulario from "./formulario"
 
 const Contact = () => {
   return (
-    <div id="contact" className="flex flex-col items-center gap-3">
+    <div id="contact" className="flex flex-col items-center gap-3 mb-32">
         <h3 className="text-2xl text-center" >Contact</h3>
         <p className="text-center w-[350px] sm:w-[450px]">Por favor, sigamos en contacto. Puedes rellenar este formulario, y nos comunicaremos vía mail.</p>
         <Formulario />
