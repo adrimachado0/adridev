92c8cbb17463df98674b6b3eefb403b1e80a5c51
adrimachado0 <lolo-6_@hotmail.com>
2023-09-14 15:33:46 -0300

    Mongodb

--- src/app/api/projects/route.js
+++ src/app/api/projects/route.js
@@ -4,13 +4,7 @@ import mongoose from "mongoose";
 import Proyectos from "../../../../models/proyectosSchema";
 
 export async function GET () {
-    try {        
-        await mongoose.connect(process.env.MONGODB_URI)
-        console.log('MONGODB CONNECTED')
-    } catch (error) {
-        NextResponse.json({message:'Mongodb no se pudo conectar'})
-        console.log(error)
-    }
+    await connectMongoDB();
     try {
         const projects = await Proyectos.find({}).exec();
         mongoose.disconnect();
