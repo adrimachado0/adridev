1d52b8588e39dc9c38b6d504be0e2ac046993cbc
adrimachado0 <lolo-6_@hotmail.com>
2023-09-14 16:17:07 -0300

    psuheando

--- components/projects/projects.jsx
+++ components/projects/projects.jsx
@@ -16,7 +16,7 @@ const Projects = () => {
   const [proyectos, setProyectos] = useState();
 
   useEffect(() => {
-    axios.get('https://adridev-ikua288bs-adrimachado0.vercel.app//api/projects')
+    axios.get('https://adridev-ikua288bs-adrimachado0.vercel.app/api/projects')
       .then(response => response.data)
       .then(data => {setProyectos(data)})
     return
